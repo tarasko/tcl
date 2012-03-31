0b405e26ec2c60d586238b06670928f400128afe
Taras Kozlov <kozlov.taras@gmail.com>
2012-04-01 02:13:47 +0400

    Safe bool implementation helper

---
+++ tests/safe_bool_test.cpp
@@ -0,0 +1,25 @@
+#include <tcl/safe_bool.hpp>
+
+#include <boost/test/auto_unit_test.hpp>
+#include <vector>
+
+using namespace tcl;
+
+namespace {
+
+template<bool ExpectedResult>
+struct tester : safe_bool< tester<ExpectedResult> >
+{
+    bool boolean_test() const
+    {
+        return ExpectedResult;
+    }
+};
+
+}                                                           // anonymous namespace
+
+BOOST_AUTO_TEST_CASE(safe_bool_test)
+{
+    BOOST_CHECK(!tester<false>());
+    BOOST_CHECK(tester<true>());
+}
